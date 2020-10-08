eda3a2223b7379d704595ee0802b0954424943f9
Devaa Pimpale <63437204+devarshi-lab@users.noreply.github.com>
2020-10-08 20:52:56 +0530

    Add files via upload

---
+++ Searching/BinarySearch.java
@@ -0,0 +1,64 @@
+//Searching the number using binary search
+
+import java.lang.*;
+import java.util.*;
+import Devarshi.MyArray;
+class Searching extends MyArray
+{
+	public Searching(int size)
+	{
+		super(size);
+	}
+
+	public boolean BinarySearch(int no)		//Binary search
+	{
+		int start = 0,middle = 0,end = (arr.length-1);
+		boolean flag = false;
+
+		while(start<=end)
+		{
+			middle = (start+end)/2;
+
+			if((arr[middle]==no) || (arr[start]==no) || (arr[end]==no))
+			{
+				flag = true;
+				break;
+			}
+			else if(arr[middle]<no)
+			{
+				start = middle+1;
+			}
+			else
+			{
+				end = middle-1;
+			}
+		}
+
+		return flag;
+	}
+}
+class BinarySearch
+{
+	public static void main(String[] args) 
+	{
+		Scanner input = new Scanner(System.in);
+		System.out.print("Enter the number elements :  \t");
+		int iNo = input.nextInt();
+
+		Searching obj = new Searching(iNo);
+		obj.Accept();
+		obj.Display();
+
+		System.out.print("Enter the element u want to search : \t");
+		int no = input.nextInt();
+
+		if(obj.BinarySearch(no))
+		{
+			System.out.println("Element is in array");
+		}
+		else
+		{
+			System.out.println("Element Not Found");
+		}
+	}
+}
